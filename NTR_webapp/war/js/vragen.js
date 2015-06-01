ac31b479f16ebf9bf49ced6e538ed7ff5acaf4ff
Lars <LarsCelie@gmail.com>
2015-06-01 15:32:25 +0200

    some changes

--- NTR_webapp/war/js/vragen.js
+++ NTR_webapp/war/js/vragen.js
@@ -2,12 +2,30 @@
  * 
  */
 
-function addQuestion(questionType){
+function addQuestion(questionType) {
 	var div = document.createElement('form');
 	$(div).addClass('form-horizontal')
 	var label = document.createElement('label')
 	$(label).text('Vul hier uw vraag in: ');
-	
-	
-	
+
+}
+
+function createSurvey() {
+	var json = {
+		"questions" : []
+	};
+
+	var cntr = 1;
+	var id = "vraag" + cntr + "type";
+	var element = document.getElementById(id);
+	while (element !== undefined) {
+		
+		
+		
+		
+		cntr++;
+		id = "vraag" + cntr + "type";
+		element = document.getElementById(id);
+	}
+
 }
