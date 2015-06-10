0cd4596fe25cab75586d812744b03307fdea560a
Lars <LarsCelie@gmail.com>
2015-06-10 13:01:44 +0200

    Working uberform.

--- NTR_webapp/war/js/uberform.js
+++ NTR_webapp/war/js/uberform.js
@@ -3,52 +3,91 @@
  */
 
 // global counter
-var counter = 0;
+var counter = 1;
 
-function addQuestion() {
-	
-	counter++;
-	var sVraag = 'vraag';
-	var sType = 'type';
-	var sValue = 'value';
-	
-	var div = document.getElementById('questionList');
-	var questionType = document.getElementById('questionSelector').value;
-	
-	var panel = document.createElement('div');
-	panel.setAttribute('class','panel panel-primary');
-	
-	var panelHeader = document.createElement('div');
-	panelHeader.setAttribute('class', 'panel-heading');		
-	panel.appendChild(panelHeader);
-	
-	var panelTitle = document.createElement('h1');
-	panelTitle.setAttribute('class', 'panel-title');
-	panelTitle.id = sVraag + counter + sType;
-	panelTitle.innerHTML = counter+ ': ' +questionType;
-	panelHeader.appendChild(panelTitle);
-	
-	var panelBody = document.createElement('div');
-	panelBody.setAttribute('class', 'panel-body');	
-	var input;
-	if(questionType == 'multipleSelect' || questionType == 'multipleChoice') {
-		input = createQuestion(false);
-	} else {
-		input = createQuestion(true);
+var questions = { "questions" : []};
+
+function showSavedQuestions(){
+	var div = document.getElementById('questionListSaved');
+	div.innerHTML = "";
+	for (var i = 0; i < questions["questions"].length; i++) {
+		var panel = createQuestion(questions["questions"][i]);
+		questions["questions"][i].index = i+1;
+		panel.removeAttribute('class');
+		panel.setAttribute('class','panel panel-success')
+		panel.id = counter;
+		div.appendChild(panel);
 	}
-	panelBody.appendChild(input);
-	panel.appendChild(panelBody);
-	div.appendChild(panel);
 }
 
-function createQuestion(normal) {
+function createDiv(className) {
+	var div = document.createElement('div');
+	div.setAttribute('class',className);
+	return div;
+}
+
+function createTitle(data, className) {
+	var h1 = document.createElement('h1');
+	h1.setAttribute('class', 'panel-title');
+	var index = (data == undefined) ? counter : data.index;
+	h1.innerHTML = "Vraag " + index;
+	return h1;
+}
+
+
+function createQuestion(data) {	
+	//var panelClass = saved ? "panel panel-success" : "panel panel-primary"; 
+	var panel = createDiv('panel panel-primary');
+	panel.id = counter; 
+	
+	var inputType = createInputElement('text','hide');
+	inputType.id = "vraag" + counter + "type";
+	if (data == undefined) {
+		inputType.value = document.getElementById('questionSelector').value;
+	}
+	panel.appendChild(inputType);
+			
+	var panelHeader = createDiv('panel-heading');
+	var panelTitle = createTitle(data, 'panel-title');
+	var type = (data == undefined) ? inputType.value : data.type; //data.type = input or form, it needs to be questionType
+	
+	panelHeader.appendChild(panelTitle);
+	
+	var questionType = createDiv('hide');
+	questionType.innerHTML = document.getElementById('questionSelector').value;
+	
+	var panelBody = createDiv('panel-body');
+	
+	var input =	(type == 'multipleSelect' || type == 'multipleChoice')  ? createInput(data, false) : createInput(data, true);
+	
+	panelBody.appendChild(input);
+	panel.appendChild(panelHeader);
+	panel.appendChild(panelBody)
+	
+	return panel;
+}
+
+function addQuestion(){
+	var div = createQuestion();
+	var resultArea = document.getElementById('questionList');
+	resultArea.innerHTML = "";
+	resultArea.appendChild(div);
+}
+
+function createInputElement(type, className){
+	var input = document.createElement('input');
+	input.type = type;
+	input.setAttribute('class', className);
+	return input;
+}
+
+function createInput(data, isNormal) {
 	
 	var sVraag = 'vraag';
 	var sType = 'type';
 	var sValue = 'value';
 	
-	var div = document.createElement('div');
-	div.setAttribute('class','row');
+	var div = createDiv('row');
 	
 	//attachment
 	var labelAttachment = document.createElement('label');
@@ -56,12 +95,11 @@ function createQuestion(normal) {
 	labelAttachment.innerHTML = "Illustratie:";
 	
 	var inputAttachmentDiv = document.createElement('div');
-	inputAttachmentDiv.setAttribute('class','col-sm-5');
+	inputAttachmentDiv.setAttribute('class','col-sm-7');
 	
-	var inputAttachment = document.createElement('input');
-	inputAttachment.type = "file";
+	var inputAttachment = createInputElement('file','file');
 	inputAttachment.accept = "file_extension|audio/*|video/*|image/*|media_type";
-	inputAttachment.placeholder = "Voeg uw bestand toe";
+	//if (data !== undefined) inputAttachment.value = data.attachment.value;
 	inputAttachmentDiv.appendChild(inputAttachment);
 	//attachment
 	
@@ -70,19 +108,39 @@ function createQuestion(normal) {
 	labelQuestion.setAttribute('class','control-label col-sm-1');
 	labelQuestion.innerHTML = "Vraag:";
 	
-	var inputDiv = document.createElement('div');
-	inputDiv.setAttribute('class','col-sm-5');
+	var inputDiv = createDiv('col-sm-5');
 	
-	var input = document.createElement('input');
-	input.type = "text";
-	input.setAttribute('class','form-control');
+	var input = createInputElement('text','form-control');
 	input.placeholder = "Vul hier de vraag in";
+	if (data !== undefined) input.value = data.value;
 	//question
 	
-//	div.appendChild(labelAttachment);
-//	div.appendChild(inputAttachmentDiv);
-//	
-//	div.appendChild(document.createElement('br'));
+	var buttonDiv = createDiv('col-sm-4');
+	
+	var buttonAdd = document.createElement('button');
+	buttonAdd.setAttribute('class','btn btn-success col-sm-5');
+	buttonAdd.type = "button";
+	if (data !== undefined) {
+		buttonAdd.onclick = changeQuestion;
+		buttonAdd.innerHTML = "Sla wijziging op";
+	} else {
+		buttonAdd.onclick = saveQuestion;
+		buttonAdd.innerHTML = "Sla vraag op";
+	}
+	
+	var buttonDelete = document.createElement('button');
+	buttonDelete.setAttribute('class','btn btn-danger col-sm-5 col-sm-offset-2');
+	buttonDelete.type = "button";
+	buttonDelete.innerHTML = "Verwijder vraag"
+	buttonDelete.onclick = deleteQuestion;
+	
+	buttonDiv.appendChild(buttonAdd);
+	buttonDiv.appendChild(buttonDelete);
+	
+	div.appendChild(labelAttachment);
+	div.appendChild(inputAttachmentDiv);
+	
+	div.appendChild(buttonDiv);
 	
 	div.appendChild(labelQuestion);
 	div.appendChild(inputDiv);
@@ -90,7 +148,7 @@ function createQuestion(normal) {
 	
 	
 	
-	if (normal) {
+	if (isNormal) {
 		input.id = sVraag+counter+sValue;
 		
 		return div;
@@ -113,29 +171,29 @@ function createQuestion(normal) {
 		
 		form.appendChild(button);
 		
+		if (data !== undefined) {
+			for (var i = 0; i < data.options.length; i++){
+				addOption(button, data.options[i]);
+			}
+		}
+		
 		return form;
 		
 	}
 	
 }
 
-function addOption(optionBtn) {
+function addOption(optionBtn, value) {
 	
-	var x = optionBtn.target.parentNode; //form
-	
-	var row = document.createElement('div');
-	row.setAttribute('class','row');
+	var row = createDiv('row')
 	
 	var label = document.createElement('label');
 	label.setAttribute('class', 'control-label col-sm-2');
 	label.innerHTML = "Optie:";
 	
-	var inputDiv = document.createElement('div');
-	inputDiv.setAttribute('class','col-sm-3');
+	var inputDiv = createDiv('col-sm-3');
 	
-	var input = document.createElement('input');
-	input.type = "text";
-	input.setAttribute('class','form-control');
+	var input = createInputElement('text','form-control');
 	input.placeholder = "Vul hier je optie in";
 	row.appendChild(document.createElement('br'));
 	
@@ -143,36 +201,69 @@ function addOption(optionBtn) {
 	row.appendChild(inputDiv);
 	inputDiv.appendChild(input);
 	
-	x.children[1].appendChild(row);
+	var form;
+	if (optionBtn.target == undefined) {
+		form = optionBtn.parentNode; //form
+		input.value = value;
+	} else {
+		form = optionBtn.target.parentNode;
+	}
+	
+	form.children[1].appendChild(row);
 }
 
 function createSurvey() {
-	
-	var json = {
-		'questions' : []
-	};
-
-	for (var i = 1; i < counter; i++){
-		var vraag = 'vraag' + i; //question
-		var id = vraag + 'value'; //input element id
-		var element = document.getElementById(id); //get the element
-		var type = element.nodeName.toLowerCase();  //get type in lower case
-		var obj = {};
-		if (type === "input"){ //open, photo, image, audio questions
-			obj["value"] = element.value;
-			obj["type"] = 
-			json.questions[(i-1)] = element.value; //value of the question input
-			
-			
-		} else if (type == "form") { //multiple select/choice
-			var length = element.elements.length;
-			var arr = [];
-			for (var x = 1; x < length; x++){
-				var inputText = element.elements[x].value;
-				arr.push(inputText);
-			}
-			obj["options"] = arr;
+	$.ajax({
+		url: "localhost:8080/NTR_application/Research/survey",
+		type: "PUT",
+		data: questions,
+		dataType: "json",
+		success: function(){
+			alert("survey toegevoegd!");
+		},
+		failure: function(){
+			alert("Er ging iets mis...");
 		}
-	}
+	});
+}
 
+function saveQuestion(){
+	var vraagid = 'vraag' + counter + 'value';
+	var element = document.getElementById(vraagid);
+	var type = element.nodeName.toLowerCase();
+	var obj = {};
+	if (type === "input"){ //open, photo, image, audio questions
+		
+		obj["value"] = element.value;
+	
+	} else if (type == "form") { //multiple select/choice 
+		var length = element.elements.length -1; //last element is add another option
+		var arr = [];
+		for (var x = 4; x < length; x++){  //start at 4th element to skip question and buttons
+			var inputText = element.elements[x].value;
+			arr.push(inputText);
+		}
+		obj["options"] = arr;
+		obj["value"] = element.elements[3].value;
+		
+	}
+	var id = "vraag" + counter + "type";
+	obj["type"] = document.getElementById(id).value;
+	obj["index"] = counter;
+	
+	questions["questions"][counter-1] = obj;
+	showSavedQuestions();
+	document.getElementById('questionList').innerHTML = "";
+	counter++;
+}
+
+function deleteQuestion(element){
+	var index = element.target.parentNode.parentNode.parentNode.parentNode.id - 1; //id of the div minus 1 for index for JSON obj
+	questions["questions"].splice(index, 1);
+	counter--;	
+	showSavedQuestions();
+}
+
+function changeQuestion(element){
+	//TODO: allow changing the element.
 }
